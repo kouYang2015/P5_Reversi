de1daae603837408cbc7df02914954f9bcb4acfa
Kou <Kou@KY>
2021-11-17 17:46:28 -0600

    findWinner() isOver() method implement

--- P5_KY7463_DH1317_Reversi/src/main/java/model/ReversiBoard.java
+++ P5_KY7463_DH1317_Reversi/src/main/java/model/ReversiBoard.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ReversiBoard implements Serializable{
@@ -19,6 +20,7 @@ public class ReversiBoard implements Serializable{
 	}
 
 	/**
+	 * 
 	 * @return the turn
 	 */
 	public int getTurn() {
@@ -33,6 +35,26 @@ public class ReversiBoard implements Serializable{
 		return (HashMap<Integer, Disks>) occupiedSpaces.clone();
 	}
 	
+	public ArrayList<Integer> findLegalMove(){
+		ArrayList<Integer> legalMoves = new ArrayList<Integer>();
+		//TODO: implement how we add indexes to legalMoves. Check each vertical, horizontal, diagonal array if it contains at least one
+		//currentPlayer color and then iterate through that array to see if any moves exists in it.
+		return legalMoves;
+	}
 	
+
+	public Disks findWinner() {
+		return blackCount < whiteCount ? Disks.WHITE : Disks.BLACK;
+	}
+	
+	/**
+	 * The game is over if there are no legal moves left or if the turn is equal to the NUM_SPACES meaning we have filled up the board.
+	 * 
+	 * @return 	true: no legal moves left (findLegalMove() array is empty) or the number of turns has reached NUM_SPACES.
+	 * 			false: there are still legal moves left and the number of turns has not reached the NUM_SPACES.
+	 */
+	public boolean isOver() {
+		return findLegalMove().isEmpty() || turn == NUM_SPACES;
+	}
 	
 }
